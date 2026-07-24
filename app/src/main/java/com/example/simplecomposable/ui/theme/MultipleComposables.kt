5209d88e4505d68a3a44344873485a78b63d926f
Gerardo Custodio <everythingissofast@gmail.com>
2026-07-24 07:54:42 -0600

    add horizontal alignment composable

--- app/src/main/java/com/example/simplecomposable/ui/theme/MultipleComposables.kt
+++ app/src/main/java/com/example/simplecomposable/ui/theme/MultipleComposables.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.simplecomposable.Greeting2
@@ -32,6 +36,28 @@ class MultipleComposables : ComponentActivity() {
     }
 }
 
+//@Composable
+//fun UserInfo() {
+//    Row {
+//        Icon(
+//            imageVector = Icons.Default.Person,
+//            contentDescription = null
+//        )
+//        Text(text = "Alex")
+//    }
+//}
+
+@Composable
+fun funAlign() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text= "Profile")
+        Button(onClick = {}){
+            Text(text = "Edit")
+        }
+    }
+}
 @Composable
 fun profileScreen2(name: String, modifier: Modifier = Modifier) {
     Column {
@@ -40,6 +66,7 @@ fun profileScreen2(name: String, modifier: Modifier = Modifier) {
             text = "Hello $name!",
             modifier = modifier
         )
+        funAlign()
     }
 }
 
