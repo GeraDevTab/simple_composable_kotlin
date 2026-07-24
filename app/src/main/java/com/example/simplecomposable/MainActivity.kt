6a1664eefc399870c84e4a396efdf831c8091a37
Gerardo Custodio <everythingissofast@gmail.com>
2026-07-24 07:04:26 -0600

    first commit simple composable kotlin

---
+++ app/src/main/java/com/example/simplecomposable/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.simplecomposable
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.simplecomposable.ui.theme.SimpleComposableTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SimpleComposableTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Greeting2() {
+    Text(text = "Hello, Jetpack Compose!")
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    SimpleComposableTheme {
+//        Greeting("Android")
+//    }
+//}
+
+@Preview(showBackground = true)
+@Composable
+fun Greeting2Preview() {
+    SimpleComposableTheme {
+        Greeting2()
+    }
+}
