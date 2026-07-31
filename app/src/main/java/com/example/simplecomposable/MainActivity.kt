b982ca21556c281899bc4879c05a38dc82ff7733
Gerardo Custodio <everythingissofast@gmail.com>
2026-07-31 09:25:24 -0600

    add counter screen

--- app/src/main/java/com/example/simplecomposable/MainActivity.kt
+++ app/src/main/java/com/example/simplecomposable/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.simplecomposable.ui.theme.Counter
 import com.example.simplecomposable.ui.theme.LoginForm
 import com.example.simplecomposable.ui.theme.SimpleComposableTheme
 
@@ -22,7 +23,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             SimpleComposableTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LoginForm()
+                    //LoginForm()
+                    Counter()
                 }
             }
         }
