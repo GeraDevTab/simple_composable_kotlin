b5ed0474a22c22a2599859feaebd56d4f6470154
Gerardo Custodio <everythingissofast@gmail.com>
2026-08-09 16:59:14 -0600

    add AsynImage at elevatedcard composable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    //coid para imagenes webp
+    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.3.0")
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
--- app/src/main/java/com/example/simplecomposable/ui/screen/MultipleComposables/ElevatedCard.kt
+++ app/src/main/java/com/example/simplecomposable/ui/screen/MultipleComposables/ElevatedCard.kt
@@ -1,6 +1,7 @@
 package com.example.simplecomposable.ui.screen.MultipleComposables
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CardDefaults
@@ -15,6 +16,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.example.simplecomposable.R
 import java.nio.file.WatchEvent
 
@@ -25,7 +27,7 @@ fun ElevatedCardExample() {
             defaultElevation = 16.dp
         ),
         modifier = Modifier
-            .size(width = 240.dp, height = 150.dp)
+            .size(width = 240.dp, height = 350.dp)
     ) {
         Text(
             text = "Elevated",
@@ -38,6 +40,12 @@ fun ElevatedCardExample() {
             contentDescription = "pihaya fruta",
             modifier = Modifier.size(80.dp)
         )
+        Spacer(modifier = Modifier)
+        AsyncImage(
+            model = "lacasadeel.net/wp-content/uploads/2025/01/alien-scaled.jpg.webp",
+            contentDescription = "image desde la nube",
+            modifier = Modifier.size(80.dp))
+
 
     }
 }
