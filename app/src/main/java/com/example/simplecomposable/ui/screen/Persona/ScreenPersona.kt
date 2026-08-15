22cf98f046adc2420ffe559434da11dbf4f9b0cf
Gerardo Custodio <everythingissofast@gmail.com>
2026-08-14 21:47:27 -0600

    add Floating Action Button

--- app/src/main/java/com/example/simplecomposable/ui/screen/Persona/ScreenPersona.kt
+++ app/src/main/java/com/example/simplecomposable/ui/screen/Persona/ScreenPersona.kt
@@ -32,6 +32,7 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
@@ -112,6 +113,10 @@ fun PersonCard(personas: List<PersonaDTO>, modifier: Modifier = Modifier) {
             }
         },
             containerColor = Color.Green)
+
+        FloatingActionButton(onClick = { print("Hello") }) {
+            Icon(Icons.Filled.Favorite, "Floating action button.")
+        }
     }
 
     @Composable
