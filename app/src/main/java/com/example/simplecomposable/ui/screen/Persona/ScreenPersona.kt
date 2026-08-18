888e49a8ec89f4cf60c06d182af1c05b8ae7116e
Gerardo Custodio <everythingissofast@gmail.com>
2026-08-18 08:10:11 -0600

    add bar loagind

--- app/src/main/java/com/example/simplecomposable/ui/screen/Persona/ScreenPersona.kt
+++ app/src/main/java/com/example/simplecomposable/ui/screen/Persona/ScreenPersona.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeightIn
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -35,15 +36,18 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.RangeSlider
@@ -147,6 +151,9 @@ fun PersonCard(personas: List<PersonaDTO>, modifier: Modifier = Modifier) {
             onCheckedChange = { checkedState.value = it }
         )
 
+        HorizontalDivider(thickness = 2.dp)
+        Spacer(modifier = Modifier.size(10.dp))
+
         val datePickerState = rememberDatePickerState()
         Column(
             modifier = modifier.fillMaxSize(),
@@ -221,6 +228,24 @@ fun PersonCard(personas: List<PersonaDTO>, modifier: Modifier = Modifier) {
                 colors = SliderDefaults.colors()
             )
         }
+
+        HorizontalDivider(thickness = 2.dp)
+
+
+        var loading by remember { mutableStateOf(false) }
+
+        Button(onClick = { loading = true }, enabled = !loading) {
+            Text("Start loading")
+        }
+
+        if (!loading) return
+
+        CircularProgressIndicator(
+            modifier = Modifier.width(64.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+
 
 
 
