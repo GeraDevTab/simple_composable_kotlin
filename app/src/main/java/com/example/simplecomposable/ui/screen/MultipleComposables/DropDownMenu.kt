2b03c81128bbea81aa4bf910787508819e074ca6
Gerardo Custodio <everythingissofast@gmail.com>
2026-08-30 12:45:42 -0600

    add preview of dropdown menu

--- app/src/main/java/com/example/simplecomposable/ui/screen/MultipleComposables/DropDownMenu.kt
+++ app/src/main/java/com/example/simplecomposable/ui/screen/MultipleComposables/DropDownMenu.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -114,4 +115,10 @@ fun DropDownMenu() {
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewDropDownMenu() {
+    DropDownMenu()
 }
